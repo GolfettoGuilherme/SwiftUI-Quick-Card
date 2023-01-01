73908106d584fe6bcad6ed9cb57c4e0b66e516c0
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2022-12-31 21:10:13 -0300

    Initial Commit

---
+++ QuickCard/QuickCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCardApp.swift
+//  QuickCard
+//
+//  Created by Guilherme Golfetto on 31/12/22.
+//
+
+import SwiftUI
+
+@main
+struct QuickCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
